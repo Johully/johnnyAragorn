47c18a4cf3e884eafa198de8fb7e4a95c815de80
JnHuang <jstyjnhuang@it888.com>
2023-09-19 15:37:07 +0800

    tuisong

---
+++ johnnyAragorn/assets/AboutView-124fa7e9.js
@@ -0,0 +1,1 @@
+import{_ as e,d as o,o as s,f as t}from"./index-a143b474.js";const c={},a={class:"about"},n=t("h1",null,"This is an about page",-1),_=[n];function r(i,d){return s(),o("div",a,_)}const l=e(c,[["render",r]]);export{l as default};
